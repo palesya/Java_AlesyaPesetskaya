e6e737a2da61b8d7cd37bea3d734705e41504afc
”Alesya <palesyav89@gmail.com>
2022-11-16 23:17:22 +0300

    controller tests

--- src/test/java/com/joint_walks/java_alesyapesetskaya/web/LoginControllerTest.java
+++ src/test/java/com/joint_walks/java_alesyapesetskaya/web/LoginControllerTest.java
@@ -1,19 +1,25 @@
 package com.joint_walks.java_alesyapesetskaya.web;
 
-import com.joint_walks.java_alesyapesetskaya.config.SpringSecurityWebAuxTestConfig;
+import com.joint_walks.java_alesyapesetskaya.config.MockSpringSecurityFilter;
+import com.joint_walks.java_alesyapesetskaya.model.Role;
+import com.joint_walks.java_alesyapesetskaya.model.User;
 import com.joint_walks.java_alesyapesetskaya.model.UserSecurity;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -22,6 +28,9 @@ class LoginControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Test
     void openLoginPage() throws Exception {
@@ -33,27 +42,40 @@ class LoginControllerTest {
 
     @Test
     void forbidOpenAdminPageForUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/admin/main").with(user("User").password("User").roles("USER")))
+        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/admin/main")
+                        .with(user("User").password("User").roles("USER")))
                 .andExpect(status().isForbidden());
     }
 
     @Test
     void forbidOpenUserPageForAdmin() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/user/main").with(user("Admin").password("Admin").roles("ADMIN")))
+        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/user/main")
+                        .with(user("Admin").password("Admin").roles("ADMIN")))
                 .andExpect(status().isForbidden());
     }
 
     @Test
     void openAdminPageForAdmin() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/admin/places").with(user("Admin").password("Admin").roles("ADMIN")))
+        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/admin/places")
+                        .with(user("Admin").password("Admin").roles("ADMIN")))
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    void openUserPageForUser() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/user/places").with(user("User").password("User").roles("USER")))
-//                .andExpect(status().isOk());
-//    }
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    @Test
+    void openUserPageForUser() throws Exception {
+        UserSecurity userDetails = new UserSecurity("Alesya","pass1",List.of("ROLE_USER"),true);
+        mockMvc.perform(MockMvcRequestBuilders.get("/dogwalker/user/dogs/")
+                        .with(user(userDetails)))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
 
     @Test
     public void redirectToLoginWhenNotAuthorized() throws Exception {
